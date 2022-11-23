d3b34087db45be4a5879c11d236eeb3b6efb2ec7
Jillian Fisher <jrfish@uw.edu>
2022-11-23 10:45:45 -0800

    change comments

--- scripts/IF_exp_wiki.sh
+++ scripts/IF_exp_wiki.sh
@@ -1,10 +1,11 @@
-# Run all approxmiation of influence function experiements
+# Run all WikiText approxmiation of influence function experiements
+# Set parameters
 lr=(1e-2 1e-2 1e-8 1e-8)
 num_epochs=(100 50 25 50)
 method=(conjugate_gradient sgd svrg arnoldi)
 n=(40 105 275 724 1903)
 
-# WikiText experiements
+# Run experiements
 for i in 0 1 2 3
 do
     python run_IF_exp.py results_dir="/results/wiki" task='wiki' iterations=5 tr_bsz=1 te_bsz=2 n=${n[0]} n_test=200 device=cuda:3 data_seed=1 approx_method=${method[i]} model.pt="/models/wiki/model_wiki_1_40_200" method.regularization_param=1 method.num_epochs=${num_epochs[i]} method.lr=${lr[i]} +model=gpt2 
--- scripts/IF_exp_zsre.sh
+++ scripts/IF_exp_zsre.sh
@@ -1,10 +1,11 @@
-# Run all approxmiation of influence function experiements
-# zsRE experiements
+# Run all zsRE approxmiation of influence function experiements
+# Set parameters
 lr=(5e-4 5e-4 5e-4 5e-4)
 num_epochs=(100 50 25 30)
 method=(conjugate_gradient sgd svrg arnoldi)
 n=(49 122 182 302 743 4499)
 
+# Run experiements
 for i in 0 1 2 3
 do
     python run_IF_exp.py results_dir="/results/zsre" task='zsre' iterations=5 tr_bsz=4 te_bsz=32 n=${n[0]} n_test=200 device=cuda:2 data_seed=1 approx_method=${method[i]} model.pt="/models/zsre/model_zsre_1_49_200" method.regularization_param=100  method.lr=${lr[i]} method.num_epochs=${num_epochs[i]} +model=bart-base
